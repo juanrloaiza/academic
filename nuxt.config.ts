527e5175c8d912fbc30c266faa512f366df4a895
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-06 16:58:49 -0300

    Adds tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/i18n'],
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss'
+  ],
   i18n: {
     vueI18n: './i18n.config.ts',
     locales: ['es', 'en'],
