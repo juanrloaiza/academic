32a5e3e4f2f4073d85d52f7617f548988ae6f2f1
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-06 16:59:02 -0300

    Makes up Bento Boxes

---
+++ components/BentoBox.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const props = defineProps({
+    cols: {
+        type: Number,
+        required: true
+    },
+    rows: {
+        type: Number,
+        required: true
+    },
+    target: String
+})
+
+// We need to use this map because Tailwind
+// doesn't like dynamically generated classes.
+// See: https://tailwindcss.com/docs/detecting-classes-in-source-files
+const colsMap: { [cols: number]: string} = {
+    1: 'col-span-1',
+    2: 'col-span-2',
+    3: 'col-span-3',
+    4: 'col-span-4',
+    5: 'col-span-5',
+    6: 'col-span-6',
+}
+
+const rowsMap: { [rows: number]: string} = {
+    1: 'row-span-1',
+    2: 'row-span-2',
+    3: 'row-span-3',
+    4: 'row-span-4',
+    5: 'row-span-5',
+    6: 'row-span-6',
+}
+
+</script>
+
+<template>
+    <div :class="`${colsMap[cols]} ${rowsMap[rows]} border min-h-5`">
+        {{ target }}
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,16 @@
+<script setup lang="ts">
+const config = useAppConfig();
+</script>
+
 <template>
-    <LangSwitcher/>
-    {{ $t('homePage') }}
+  <LangSwitcher />
+  {{ config.title }}
+  <div class="grid grid-cols-12 grid-flow-dense min-h-full gap-4">
+    <BentoBox :cols=2 :rows=2 target="about"></BentoBox>
+    <BentoBox :cols=4 :rows=2 target="cv"></BentoBox>
+    <BentoBox :cols=6 :rows=1 target="pubs"></BentoBox>
+    <BentoBox :cols=4 :rows=2 target="talks"></BentoBox>
+    <BentoBox :cols=2 :rows=2 target="teaching"></BentoBox>
+    <BentoBox :cols=6 :rows=1 target="other"></BentoBox>
+  </div>
 </template>
