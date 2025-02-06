71826b61ef1eaa8fa49b990d23e8429257061d49
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-06 15:14:22 -0300

    Adds i18n

---
+++ i18n.config.ts
@@ -0,0 +1,8 @@
+export default defineI18nConfig(() => ({
+    legacy: false,
+    locale: 'es',
+    messages: {
+        en: {},
+        es: {}
+    }
+}))
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    vueI18n: './i18n.config.ts',
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    strategy: 'prefix'
+  }
 })
