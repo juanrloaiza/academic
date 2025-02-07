223dabb95c946fc52284b517f62cb1decfca5193
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-07 15:55:59 -0300

    Linting

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,7 @@ const config = useAppConfig();
 
 <template>
   <div class="min-h-[80vh] flex items-center">
-
-  <div class="h-60vh my-6 grid grid-cols-12 grid-flow-dense gap-4">
+    <div class="h-60vh my-6 grid grid-cols-12 grid-flow-dense gap-4">
       <BentoBox color="green" :cols="3" :rows="1" target="about"></BentoBox>
       <BentoBox color="blue" :cols="4" :rows="1" target="cv"></BentoBox>
       <BentoBox color="yellow" :cols="5" :rows="1" target="pubs"></BentoBox>
@@ -14,5 +13,4 @@ const config = useAppConfig();
       <BentoBox color="red" :cols="4" :rows="1" target="other"></BentoBox>
     </div>
   </div>
-
 </template>
