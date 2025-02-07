f43b5a656e61b618b524a541fc60b9a4542b7fd9
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-07 16:09:33 -0300

    Separates forthcoming from other publications

--- pages/pubs.vue
+++ pages/pubs.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 // Load Data
 const { data: pubs } = await useAsyncData(() =>
-  queryCollection("publications").order("year", "DESC").all()
+  queryCollection("publications")
+  .where('year', '>', '2000') // Separate Forthcoming
+  .order("year", "DESC")
+  .all()
 );
 
+const { data: forthcoming } = await useAsyncData(() =>
+  queryCollection("publications")
+  .where('year', '<', '2000') // Separate Forthcoming
+  .all()
+);
 
 </script>
 
 <template>
   <PageTitle :title="$t('pubs.title')" />
+
+  <Article v-for="(pub, index) in forthcoming" :pub="pub" :key="index" class="mb-2" />
+
   <Article v-for="(pub, index) in pubs" :pub="pub" :key="index" class="mb-2" />
     
 </template>
