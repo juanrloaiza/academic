512acce8c57e923a13411ebcf21c06019e444e55
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-07 01:31:13 -0300

    Updates tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
-  modules: [
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/content',
-  ],
+  css: ['/assets/css/main.css'],
+  modules: ["@nuxtjs/i18n", "@nuxt/content"],
   i18n: {
-    vueI18n: './i18n.config.ts',
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
-    strategy: 'prefix'
+    vueI18n: "./i18n.config.ts",
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    strategy: "prefix",
   },
-  tailwindcss: {
-    config: 
-    {
-      content: [
-        "./pages/**/*.{vue,html,js}",
-        "./components/**/*.{vue,html,js}",
-      ],
-      theme: {
-
-      }
-    }
+  vite: {
+    plugins: [tailwindcss()]
   }
-})
+  // tailwindcss: {
+  //   config: {
+  //     content: [
+  //       "./pages/**/*.{vue,html,js}",
+  //       "./components/**/*.{vue,html,js}",
+  //     ],
+  //     theme: {
+  //     }
+  //   },
+  // },
+});
