2e6f7753607ee131b278f315d6c15b33cd86c28f
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-10 19:45:00 -0300

    Make links highlighted in top bar

--- components/SocialMediaButtons.vue
+++ components/SocialMediaButtons.vue
@@ -8,33 +8,33 @@ useHead({
   <div class="mx-4 justify-self-end">
     <NuxtLink to="https://hcommons.social/@juanrloaiza" class="mx-2"
       ><i
-        class="fa-brands fa-mastodon text-2xl text-gray-800/70 hover:text-gray-800"
+        class="fa-brands fa-mastodon text-2xl text-gray-800 hover:text-primary"
       ></i
     ></NuxtLink>
     <NuxtLink
       to="https://www.researchgate.net/profile/Juan_Loaiza5"
-      class="mx-2 text-2xl text-gray-800/70 hover:text-gray-800"
+      class="mx-2 text-2xl text-gray-800 hover:text-primary"
       ><i
-        class="ai ai-researchgate text-2xl text-gray-800/70 hover:text-gray-800"
+        class="ai ai-researchgate text-2xl text-gray-800 hover:text-primary"
       ></i
     ></NuxtLink>
     <NuxtLink
       to="https://orcid.org/0000-0003-0570-3832"
-      class="mx-2 text-2xl text-gray-800/70 hover:text-gray-800"
-      ><i class="ai ai-orcid text-2xl text-gray-800/70 hover:text-gray-800"></i
+      class="mx-2 text-2xl text-gray-800 hover:text-primary"
+      ><i class="ai ai-orcid text-2xl text-gray-800 hover:text-primary"></i
     ></NuxtLink>
     <NuxtLink
       to="https://www.instagram.com/loaiza.filosofia"
-      class="mx-2 text-2xl text-gray-800/70 hover:text-gray-800"
+      class="mx-2 text-2xl text-gray-800 hover:text-primary"
       ><i
-        class="fa-brands fa-instagram text-2xl text-gray-800/70 hover:text-gray-800"
+        class="fa-brands fa-instagram text-2xl text-gray-800 hover:text-primary"
       ></i
     ></NuxtLink>
     <NuxtLink
       to="https://github.com/juanrloaiza"
-      class="mx-2 text-2xl text-gray-800/70 hover:text-gray-800"
+      class="mx-2 text-2xl text-gray-800 hover:text-primary"
       ><i
-        class="fa-brands fa-github text-2xl text-gray-800/70 hover:text-gray-800"
+        class="fa-brands fa-github text-2xl text-gray-800 hover:text-primary"
       ></i
     ></NuxtLink>
   </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@ const config = useAppConfig();
       <div class="block text-center md:flex md:justify-between items-center min-h-[5rem]">
         <div>
           <NuxtLinkLocale to="/" class="md:flex items-center">
-            <h1 class="block mb-1 md:inline md:m-0 text-4xl font-extrabold">{{ config.title }}</h1>
+            <h1 class="block mb-1 md:inline md:m-0 text-4xl font-extrabold hover:text-primary">{{ config.title }}</h1>
             <span class="hidden md:inline text-2xl mx-4 text-gray-600"> · </span>
             <span class="text-2xl text-gray-600 font-heading mt-1">Académico</span>
           </NuxtLinkLocale>
