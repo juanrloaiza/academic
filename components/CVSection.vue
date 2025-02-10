89a74f6e27f4633c85d19f81140bab5dc3edd036
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-10 17:42:52 -0300

    Abstracts CV Section

---
+++ components/CVSection.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  startYear: { type: Number, required: true },
+  endYear: { type: Number },
+});
+</script>
+
+<template>
+  <div class="grid grid-cols-8 items-baseline mb-4">
+    <div class="col-span-1 text-base">
+      <CVDates :start="startYear" :end="endYear" />
+    </div>
+    <div class="col-span-7">
+      <slot />
+    </div>
+  </div>
+</template>
--- pages/cv.vue
+++ pages/cv.vue
@@ -2,82 +2,80 @@
 const { locale } = useI18n();
 
 const { data: education } = await useAsyncData(() =>
-	queryCollection('cv')
-		.where('type', '=', 'education')
-		.order('startYear', 'DESC')
-		.all()
+  queryCollection("cv")
+    .where("type", "=", "education")
+    .order("startYear", "DESC")
+    .all()
 );
 
 const { data: employment } = await useAsyncData(() =>
-	queryCollection('cv')
-		.where('type', '=', 'employment')
-		.order('startYear', 'DESC')
-		.all()
+  queryCollection("cv")
+    .where("type", "=", "employment")
+    .order("startYear", "DESC")
+    .all()
 );
 
 const { data: service } = await useAsyncData(() =>
-	queryCollection('cv')
-		.where('type', '=', 'service')
-		.order('startYear', 'DESC')
-		.all()
+  queryCollection("cv")
+    .where("type", "=", "service")
+    .order("startYear", "DESC")
+    .all()
 );
 </script>
 
 <template>
-	<PageTitle title="CV" />
+  <PageTitle title="CV" />
 
-	<h3 class="font-bold text-2xl mb-3">{{ $t('education') }}</h3>
+  <h3 class="font-bold text-2xl mb-3">{{ $t("education") }}</h3>
+  <CVSection
+    v-for="ed in education"
+    :startYear="ed.startYear"
+    :endYear="ed.endYear"
+  >
+    <strong class="font-semibold">{{ ed.role[locale] }}</strong>
+    <br />
+    {{ ed.faculty }}, {{ ed.institution }}
+    <br />
+    <span class="text-base">
+      {{ $t("thesis") }}: "{{ ed.thesis.title }}"
+      <br />
+      {{ $t("supervisors") }}: {{ ed.thesis.supervisors?.join(", ") }}
+      <br />
+      <span v-if="ed.thesis.examiners">
+        {{ $t("examiners") }}: {{ ed.thesis.examiners?.join(", ") }}
+        <br />
+      </span>
+      {{ $t("qualification") }}:
+      {{ ed.thesis.qualification }}
+    </span>
+  </CVSection>
 
-	<div v-for="ed in education" class="grid grid-cols-8 items-baseline mb-4">
-		<div class="col-span-1 text-base">
-			<CVDates :start="ed.startYear" :end="ed.endYear" />
-		</div>
-		<div class="col-span-7">
-			<strong class="font-semibold">{{ ed.role[locale] }}</strong>
-			<br />
-			{{ ed.faculty }}, {{ ed.institution }}
-			<br />
-			<span class="text-base">
-				{{$t('thesis')}}: "{{ ed.thesis.title }}"
-				<br />
-				{{$t('supervisors')}}: {{ ed.thesis.supervisors?.join(', ') }}
-				<br />
-				<span v-if="ed.thesis.examiners">
-					{{$t('examiners')}}: {{ ed.thesis.examiners?.join(', ') }}
-					<br />
-				</span>
-				{{$t('qualification')}}:
-				{{ ed.thesis.qualification }}
-			</span>
-		</div>
-	</div>
+  <h3 class="font-bold text-2xl mb-3">{{ $t("employment") }}</h3>
 
-	<h3 class="font-bold text-2xl mb-3 mt-8">{{ $t('employment') }}</h3>
+  <CVSection
+    v-for="emp in employment"
+    :startYear="emp.startYear"
+    :endYear="emp.endYear"
+  >
+    <strong class="font-semibold">{{ emp.role[locale] }}</strong>
+    <br />
+    {{ emp.faculty }}, {{ emp.institution }}
+    <br />
+  </CVSection>
 
-	<div v-for="emp in employment" class="grid grid-cols-8 items-baseline mb-4">
-		<div class="col-span-1 text-base">
-			<CVDates :start="emp.startYear" :end="emp.endYear" />
-		</div>
-		<div class="col-span-7">
-			<strong class="font-semibold">{{ emp.role[locale] }}</strong>
-			<br />
-			{{ emp.faculty }}, {{ emp.institution }}
-			<br />
-		</div>
-	</div>
+  <h3 class="font-bold text-2xl mb-3 mt-8">{{ $t("service") }}</h3>
 
-	<h3 class="font-bold text-2xl mb-3 mt-8">{{ $t('service') }}</h3>
-
-	<div v-for="srv in service" class="grid grid-cols-8 items-baseline mb-4">
-		<div class="col-span-1 text-base">
-      <CVDates :start="srv.startYear" :end="srv.endYear" />
-		</div>
-		<div class="col-span-7">
-			<strong class="font-semibold">{{ srv.role[locale] }}</strong>
-			<br />
-			<span v-if="srv.faculty">{{ srv.faculty }},</span>
-			{{ srv.institution }}
-			<br />
-		</div>
-	</div>
+  <CVSection
+    v-for="srv in service"
+    :startYear="srv.startYear"
+    :endYear="srv.endYear"
+  >
+    <div class="col-span-7">
+      <strong class="font-semibold">{{ srv.role[locale] }}</strong>
+      <br />
+      <span v-if="srv.faculty">{{ srv.faculty }},</span>
+      {{ srv.institution }}
+      <br />
+    </div>
+  </CVSection>
 </template>
