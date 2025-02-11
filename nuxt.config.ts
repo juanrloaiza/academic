cd490672273aab035ed35a6c20b3e46b40e87e63
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-10 23:48:06 -0300

    Adds i18n base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,7 @@ export default defineNuxtConfig({
   },
   i18n: {
     vueI18n: "./i18n.config.ts",
+    baseUrl: "https://www.juanrloaiza.com",
     locales: ["es", "en"],
     defaultLocale: "es",
     strategy: "prefix",
