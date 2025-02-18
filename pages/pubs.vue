a386460cc297a0d4abd4e579ccb87fc3a3dc0f18
Juan R. Loaiza <jloaiza@uahurtado.cl>
2025-02-18 18:17:51 -0300

    Adds margin to pubs

--- pages/pubs.vue
+++ pages/pubs.vue
@@ -43,14 +43,14 @@ const { data: forthcoming } = await useAsyncData(() =>
   <PageTitle :title="$t('pubs.title')" />
 
   <h3 class="text-2xl font-bold mt-6 mb-2">{{ $t('articles') }}</h3>
-  <Article v-for="(pub, index) in forthcoming" :pub="pub" :key="`pub-${index}-f`" class="mb-2" />
+  <Article v-for="(pub, index) in forthcoming" :pub="pub" :key="`pub-${index}-f`" class="mb-4 md:mb-2" />
 
-  <Article v-for="(pub, index) in pubArticles" :pub="pub" :key="`pub-${index}-p`" class="mb-2" />
+  <Article v-for="(pub, index) in pubArticles" :pub="pub" :key="`pub-${index}-p`" class="mb-4 md:mb-2" />
 
-  <h3 class="text-2xl font-bold mt-6 mb-2">{{ $t('bookchapters') }}</h3>
-  <BookChapter v-for="(pub, index) in pubChapters" :pub="pub" :key="`pub-${index}-f`" class="mb-2" />
+  <h3 class="text-2xl font-bold mt-6 mb-4 md:mb-2">{{ $t('bookchapters') }}</h3>
+  <BookChapter v-for="(pub, index) in pubChapters" :pub="pub" :key="`pub-${index}-f`" class="mb-4 md:mb-2" />
 
-  <h3 class="text-2xl font-bold mt-6 mb-2">{{ $t('monographs') }}</h3>
-  <Book v-for="(pub, index) in pubBooks" :pub="pub" :key="`pub-${index}-f`" class="mb-2" />
+  <h3 class="text-2xl font-bold mt-6 mb-4 md:mb-2">{{ $t('monographs') }}</h3>
+  <Book v-for="(pub, index) in pubBooks" :pub="pub" :key="`pub-${index}-f`" class="mb-4 md:mb-2" />
     
 </template>
