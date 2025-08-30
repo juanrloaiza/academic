dcc8148995a66d201cc21ccf85170789202015db
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-08-30 11:00:34 -0400

    Updates Phil Sci Syllabus

--- app/components/LinkButton.vue
+++ app/components/LinkButton.vue
@@ -7,7 +7,7 @@ defineProps({
 <template>
   <a :href="to"
     ><div
-      class="inline-block mt-2 text-primary/70 hover:text-accent py-1 px-2 rounded-md border-1 text-sm"
+      class="inline-block mt-2 text-primary/70 hover:text-accent py-1 px-2 mr-2 rounded-md border-1 text-sm"
     >
       <slot />
     </div>
