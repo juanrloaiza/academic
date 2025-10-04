82d6f8a8a06fcc5a72fe63e789b72d3f8d60a90a
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-10-04 16:24:29 -0300

    Implements blog rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  site: "https://www.juanrloaiza.com/",
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
     routing: {
-      prefixDefaultLocale: true
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false
     }
   },
   vite: { plugins: [tailwindcss()], },
